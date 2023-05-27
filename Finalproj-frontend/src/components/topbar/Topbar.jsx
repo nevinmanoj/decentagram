b89270046156cfb2f76adff26f4937846e971bde
nevinmanoj <NEVINMANOJP@GMAIL.COM>
2023-05-28 03:40:49 +0530

    name changed

--- Finalproj-frontend/src/components/topbar/Topbar.jsx
+++ Finalproj-frontend/src/components/topbar/Topbar.jsx
@@ -15,7 +15,7 @@ export default function Topbar() {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo">BlockChain</span>
+        <span className="logo">Decentagram</span>
       </div>
       <div className="topbarCenter">
         {/* <div className="searchbar">
